64fa50b34828ef25267b3adec3895d745b22003b
HenokZewdie <javabcamp@gmail.com>
2017-06-22 22:46:31 -0400

    change create to update and display

--- src/main/java/DavePackage/HouseController.java
+++ src/main/java/DavePackage/HouseController.java
@@ -5,18 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Iterator;
 
 
 @Controller
 public class HouseController {
-
+    String name;
     @Autowired
     HouseRepository houseRepository;
 
     @RequestMapping(value = "/home", method = RequestMethod.GET) //@GetRequest
     public String getIt(Model model){
-        System.out.println("GET TEst");
+        //System.out.println(name);
         model.addAttribute(new House());
 
         return  "home";
@@ -24,13 +26,18 @@ public class HouseController {
 
     @RequestMapping(value = "/home", method = RequestMethod.POST)
     public String ProcessHouse(@ModelAttribute House house){
-        System.out.println("GET TEst");
+        name = house.getOwnerName();
+        System.out.println(name);
         houseRepository.save(house);
         return "redirect:/display";
     }
 
-    @GetMapping(path = "/display")
-    public @ResponseBody Iterable<House> getAllHouses(){
-        return houseRepository.findAll();
+    @RequestMapping(value = "/display", method = RequestMethod.GET)
+    public String toSend(@ModelAttribute House house, Model model){
+        Iterable<House> values = houseRepository.findByOwnerName(name);
+        System.out.println("jsfh");
+        model.addAttribute("values", values);
+
+        return "display";
     }
 }
--- src/main/java/DavePackage/HouseRepository.java
+++ src/main/java/DavePackage/HouseRepository.java
@@ -1,9 +1,10 @@
 package DavePackage;
 
 import org.springframework.data.repository.CrudRepository;
-
+import java.util.List;
 /**
  * Created by student on 6/21/17.
  */
 public interface HouseRepository extends CrudRepository<House, Long> {
+    List<House> findByOwnerName(String owner);
 }
